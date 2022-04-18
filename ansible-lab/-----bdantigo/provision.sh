aa98f01d84cfb5fdbd60b6906211aabcc8f1e118
unknown <bevuti@gmail.com>
2022-04-18 17:09:59 -0300

    adicionado lab ansiblew e gitignore

---
+++ ansible-lab/-----bdantigo/provision.sh
@@ -0,0 +1,5 @@
+#bin/sh
+
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKowCRwW+Pl9l+90KbaIRhtlFtp7IXx24uIy7WAXLWn3sb94RAjtRw8Kr/Dyzl1IAh9fdJkYU01AaTKnWHpcLOkGdOyY9hBRC2x46THNyTiw7B5sW5vgnnmk/+LmNYIp+Ju5lBMYuxMb/tnVsLCIJLs+p2dKGie39nStqAyBc9rqDFdV7Ig2fk1wEUbJ8JRtqM4wG6xpt6y40lPIXtFESukIF8Phvudib54sb3/oTDeDUjSDompRNUG59adAT0wtYlz1FprxbVLwRh6McVRhmKcrTdRQ4KMOV4gCF2aEPueWILCMpkqbLkfQ9292rQvFtXushFoxklGSb0cusHuX89 vagrant@control-node
+EOT
